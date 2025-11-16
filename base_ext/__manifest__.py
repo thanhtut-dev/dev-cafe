58f36ffe69610a85ad7551f38f08d7e1d1bfda00
Than Htut <saithanhtut.ml@gmail.com>
2025-11-16 18:31:22 +0700

    Initial commit for base_ext modules

---
+++ base_ext/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name': 'Base Module Extension',
+    'version': '1.0',
+    'description': """
+This module includes a number of base modules.
+================================================================================
+
+Shows you a list of applications features to install from.
+
+    """,
+    'depends': ['base', 'web'],
+    'data': [
+        ],
+    'installable': True,
+}
